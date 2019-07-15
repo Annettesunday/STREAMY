fdd5b8c48dbf3c14a6d1224b03a56cba76f97ced
annettesunday <annette.odhiambo@andela.com>
2019-07-15 19:08:08 +0300

    ch(StreamEdit): selecting records from state

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
+const StreamEdit = props => {
+  console.log(props);
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(mapStateToProps)(StreamEdit);
