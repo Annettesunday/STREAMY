82baf10468deccf06b996568ab049b132ae1c2fb
annettesunday <annette.odhiambo@andela.com>
2019-07-15 12:43:23 +0300

    ch(src): redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import reactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 reactDOM.render(
-    <App />,
-    document.querySelector("#root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
