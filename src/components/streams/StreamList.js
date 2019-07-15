a87a5b0c6374a00c61e6f74e959c07a367095849
annettesunday <annette.odhiambo@andela.com>
2019-07-15 17:36:02 +0300

    ch(StreamList): rendering all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,12 +6,37 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderedList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Stream</h2>
+        <div className="ui celled list">{this.renderedList()}</div>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams)
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
