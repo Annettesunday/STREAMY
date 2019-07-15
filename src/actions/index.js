c84814dc41e0c2ed741559fceeebdf201ea0641f
annettesunday <annette.odhiambo@andela.com>
2019-07-15 16:35:07 +0300

    ch(actions): bulk action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./actionTypes";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM
+} from "./actionTypes";
 
 export const signIn = userId => {
   return {
@@ -19,5 +27,37 @@ export const createStream = formValues => async dispatch => {
   dispatch({
     type: CREATE_STREAM,
     payload: response.data
+  });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({
+    type: FETCH_STREAMS,
+    payload: response.data
+  });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/stream/${id}`);
+  dispatch({
+    type: FETCH_STREAM,
+    payload: response.data
+  });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/stream/${id}`, formValues);
+  dispatch({
+    type: EDIT_STREAM,
+    payload: response.data
+  });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/stream/${id}`);
+  dispatch({
+    type: DELETE_STREAM,
+    payload: id
   });
 };
