0f48c03cb2b6fe9e3d833228153b7cdd5a12ca09
annettesunday <annette.odhiambo@andela.com>
2019-07-15 16:59:56 +0300

    ch(streamReducer): handling fetching, creating and updating

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import {
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from "../actions/actionTypes";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
