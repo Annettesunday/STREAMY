981d029ac550a4a0051bc4d6195b1ed0280d5f18
annettesunday <annette.odhiambo@andela.com>
2019-07-15 18:25:14 +0300

    ch(App): create a broswer history object

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamShow from "./streams/StreamShow";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <div>
           <Route exact path="/" component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => {
           <Route exact path="/streams/delete" component={StreamDelete} />
           <Route exact path="/streams/show" component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
