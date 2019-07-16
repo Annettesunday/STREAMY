3847ad75b3c7b3a45b059c889da9a8517415c385
annettesunday <annette.odhiambo@andela.com>
2019-07-16 11:03:22 +0300

    ch(Modal): making the modal reusable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,13 +12,12 @@ const Modal = props => {
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Stream</div>
+        <div className="header">{props.title}</div>
         <div className="content">
-          Are you sure you want to delete this stream
+          {props.content}
         </div>
         <div className="actions">
-          <button className="ui button primary">Delete</button>
-          <button className="ui button">Cancel</button>
+          {props.actions}
         </div>
       </div>
     </div>,
