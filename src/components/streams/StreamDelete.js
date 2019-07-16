c822be90c15ad22d43fdbdba4736e6cfd2d9bcba
annettesunday <annette.odhiambo@andela.com>
2019-07-16 11:08:02 +0300

    ch(StreamDelete): React Fragments

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,23 +2,22 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
-
-  const actions  = (
-    <div>
+  const actions = (
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button">Cancel</button>
-    </div>
-  )
+    </React.Fragment>
+  );
   return (
-  <div>
-  StreamDelete
-  <Modal 
-  title="Delete Stream"
-  content="Are you sure you want to delete this stream"
-  actions={actions}
-
-  />
-  </div>);
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
