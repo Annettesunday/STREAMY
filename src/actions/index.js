840e4699f957f83e9c8ba9d1130ccce43c2064bc
annettesunday <annette.odhiambo@andela.com>
2019-07-16 11:26:48 +0300

    ch(StreamDelete): fectching deleting stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -60,7 +60,7 @@ export const editStream = (id, formValues) => async dispatch => {
 };
 
 export const deleteStream = id => async dispatch => {
-  await streams.delete(`/stream/${id}`);
+  await streams.delete(`/streams/${id}`);
   dispatch({
     type: DELETE_STREAM,
     payload: id
